69ccd5ae9d7fce72cc9348b04f33a8dd01c541c2
1da <drakeleung42@gmail.com>
2022-11-28 01:47:48 +0800

    feat: + fd global ignore

--- .1da/main.sh
+++ .1da/main.sh
@@ -14,11 +14,11 @@ prompt pure
 
 # alias
 alias vf="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
-alias vfa="fd --type f --hidden --exclude .git --exclude node_modules --exclude go/pkg . ~ | fzf-tmux -p --reverse | xargs nvim"
+alias vfa="fd --type f --hidden . ~/ | fzf-tmux -p --reverse | xargs nvim"
 alias vd="fd --type d -d 1 . ~/Codes/echoingtech | fzf-tmux -p --reverse | xargs nvim"
-alias vda="fd --type d --exclude node_modules --exclude .git --exclude go/pkg . ~ | fzf-tmux -p --reverse | xargs nvim"
+alias vda="fd --type d --hidden . ~/ | fzf-tmux -p --reverse | xargs nvim"
 
 alias nr="npm run"
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias k=kubectl
 
