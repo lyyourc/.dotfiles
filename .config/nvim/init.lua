f0a9d18213b0956a30f503082ce237d2ee1f4229
1da <drakeleung42@gmail.com>
2023-04-19 23:42:03 +0800

    refactor(nvim): format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,16 +2,25 @@ require("core.options")
 
 -- package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+	vim.fn.system(
+		{
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable",
+			lazypath
+		}
+	)
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-    { import = 'core.plugins' },
-    { import = 'custom.plugins' }
+	{ import = 'core.plugins' },
+	{ import = 'custom.plugins' }
 }, {})
 
 require("core.keymaps")
--- .config/nvim/lua/core/plugins/telescope.lua
+++ .config/nvim/lua/core/plugins/telescope.lua
@@ -1,25 +1,23 @@
-return {{
-    'nvim-telescope/telescope.nvim',
-    dependencies = {'nvim-lua/plenary.nvim'},
-    config = function()
-        require('telescope').setup({
-            defaults = {
-                layout_strategy = 'horizontal',
-                -- https://www.reddit.com/r/neovim/comments/r22xrq/in_nvimtelescope_how_can_i_make_the_display_a/
-                layout_config = {
-                    prompt_position = 'top'
-                },
-                sorting_strategy = 'ascending'
-            },
-
-            -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
-            pickers = {
-                find_files = {
-                    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-                    find_command = {"rg", "--files", "--hidden", "--glob", "!**/.git/*"}
-                }
-            }
-        })
-
-    end
-}}
+return { {
+	'nvim-telescope/telescope.nvim',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		require('telescope').setup({
+			defaults = {
+				layout_strategy = 'horizontal',
+				-- https://www.reddit.com/r/neovim/comments/r22xrq/in_nvimtelescope_how_can_i_make_the_display_a/
+				layout_config = {
+					prompt_position = 'top'
+				},
+				sorting_strategy = 'ascending'
+			},
+			-- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
+			pickers = {
+				find_files = {
+					-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+				}
+			}
+		})
+	end
+} }
